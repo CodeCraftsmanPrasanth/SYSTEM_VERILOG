6d285c0b041c0d4492a6110eb578b562d3023d57
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-09-19 09:50:28 +0530

    Create 1_DIMENSION.sv

---
+++ DAY1/DYNAMIC_ARRAY/1_DIMENSION.sv
@@ -0,0 +1,9 @@
+module dynamic;
+  int array [];
+  initial begin
+    array='{$random,$random,$random,$random,$random,$random,$random};
+    $display("array = %p",array);
+  end
+endmodule
+
+array = '{303379748, -1064739199, -2071669239, -1309649309, 112818957, 1189058957, -1295874971}
