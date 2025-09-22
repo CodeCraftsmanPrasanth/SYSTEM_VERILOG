0796664ecbbfb63a71e247c93e3fa1922dae4512
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-09-22 20:26:55 +0530

    Create DA_TO_AA.sv

---
+++ DAY1/ASSOCIATIVE_ARRAY/DA_TO_AA.sv
@@ -0,0 +1,10 @@
+module array2;
+  int fruits [] [string];
+  initial begin
+    fruits = new [2];
+    fruits [0] = '{ "apple" : 1, "grape" : 2 };
+    fruits [1] = '{ "melon" : 3, "cherry" : 4 };
+    foreach (fruits[i,fruit])  $display ("fruits[%0d][%s] = %0d", i, fruit, fruits[i][fruit]);
+
+  end
+endmodule
