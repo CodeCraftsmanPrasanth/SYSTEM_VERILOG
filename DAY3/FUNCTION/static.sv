b7b04072e4eb54c365e52d88ad8275e1edee12e1
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-10-17 12:39:22 +0530

    Add static.sv with adder function and test cases

---
+++ DAY3/FUNCTION/static.sv
@@ -0,0 +1,14 @@
+module functionex;
+  int a,b,c;
+  function  [31:0] adder();
+    int a;
+    a++;
+    return a;
+  endfunction
+  
+  initial begin
+    $display("%d",adder());
+     $display("%d",adder());
+     $display("%d",adder());
+  end
+endmodule
