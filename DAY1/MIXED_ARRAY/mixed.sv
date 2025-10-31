fa2a264a4f2cbd2b64ffe2669f24c3bcf4b24228
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-10-31 16:33:01 +0530

    Delete DAY1 directory

--- DAY1/MIXED_ARRAY/mixed.sv
+++
@@ -1,9 +0,0 @@
-module mixedarray; 
-  logic [0:1][0:3] array [5][4][3]; 
-  initial begin 
-    foreach (array[i,k,l,m]) begin 
-      array[i][k][l][m]=$random; 
-    end 
-    $display("array = %p",array); 
-  end 
-endmodule
