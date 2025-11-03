3bdd7805e1fe2a7ad5310ce611d82dd30ac3f475
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-03 09:52:00 +0530

    Create assign1.sv

---
+++ 05.CLASS/CLASS_ASSIGNMENT/assign1.sv
@@ -0,0 +1,30 @@
+// Code your design here
+class assigm;
+  int a,b;
+  function void display(string c);
+    $display("%s value of the a=%0d, value of the b=%0d",c, a,b);
+  endfunction
+endclass
+
+module assignment_example;
+  assigm p1,p2;
+  initial begin
+    p1=new();
+    p1.a=$urandom_range(1,100);
+    p1.b=$urandom_range(1,100);
+    p1.display("p1");
+    p2=p1;
+    p2.display("p2");
+    p2.a=$urandom_range(1,100);
+    p2.b=$urandom_range(1,100);
+    p1.display("p1");
+    p2.display("p2");
+  end
+endmodule
+    
+    
+    
+    
+    
+    
+    
