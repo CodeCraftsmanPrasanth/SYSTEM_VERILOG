c799182818b2963d934485136e1e2876cc35dc47
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-17 11:02:42 +0530

    Implement class hierarchy with protected members
    The code defines a class hierarchy with protected members and methods. An error is noted for illegal access to a protected member from outside its class.

---
+++ 16.DATA_HIDING&ENCAPSULATION/PROTECTED/methoderror.sv
@@ -0,0 +1,28 @@
+class Brand;
+  protected string brand;
+  protected function void displayb();
+    $display("BRAND : %0s",brand);
+  endfunction
+endclass
+
+class Model extends Brand;
+  string model;
+  function new(string brand,model);
+    this.model=model;
+    this.brand=brand;
+  endfunction
+  function void display();
+    displayb();
+    $display("MODEL : %s",model);
+  endfunction
+endclass
+
+module bike;
+  initial begin
+    Model m;
+    m=new("HERO","XPULSE 200 4v");
+    m.displayb();
+  end
+endmodule
+
+** Error (suppressible): (vlog-8688) design.sv(24): Illegal access to protected member displayb.
