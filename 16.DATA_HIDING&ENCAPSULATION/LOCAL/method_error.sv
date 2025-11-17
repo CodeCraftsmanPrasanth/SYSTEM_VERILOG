1e790d8127ef5f4318d3a7ef0734e57b21c8e6fd
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-17 10:40:18 +0530

    Create method_error.sv

---
+++ 16.DATA_HIDING&ENCAPSULATION/LOCAL/method_error.sv
@@ -0,0 +1,20 @@
+class Brand;
+  local string brand;
+  function new(string brand);
+    this.brand=brand;
+    display();
+  endfunction
+  local function void display();
+    $display("BRAND : %0s",brand);
+  endfunction
+endclass
+
+module bike;
+  initial begin
+    Brand b;
+    b=new("HERO");
+    b.display();
+  end
+endmodule
+
+** Error (suppressible): (vlog-8688) design.sv(18): Illegal access to local member display.
