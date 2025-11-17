8f9b4f85e8a2979c1f853c5986de986b6a4647cb
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-17 10:49:27 +0530

    Create properties.sv

---
+++ 16.DATA_HIDING&ENCAPSULATION/PROTECTED/properties.sv
@@ -0,0 +1,19 @@
+class Brand;
+  protected string brand;
+  function new(string brand);
+    this.brand=brand;
+  endfunction
+  function void display();
+    $display("BRAND : %0s",brand);
+  endfunction
+endclass
+
+module bike;
+  initial begin
+    Brand b;
+    b=new("HERO");
+    b.display();
+  end
+endmodule
+
+# BRAND : HERO
