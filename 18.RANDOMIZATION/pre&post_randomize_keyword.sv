804d5624a79a63100f6164d07708b55a980436cf
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-21 10:41:22 +0530

    Create pre&post_randomize_keyword.sv

---
+++ 18.RANDOMIZATION/pre&post_randomize_keyword.sv
@@ -0,0 +1,19 @@
+class random;
+  rand bit [7:0] a;
+  randc bit [7:0] b;
+  
+  function void pre_randomize();
+    $display("pre_randomize: a=%0d,b=%0d",a,b);
+  endfunction
+  function void post_randomize();
+    $display("post_randomize: a=%0d,b=%0d",a,b);
+  endfunction
+endclass
+
+module random_ex;
+  random ra;
+  initial begin
+    ra=new();
+    ra.randomize();
+  end
+endmodule
