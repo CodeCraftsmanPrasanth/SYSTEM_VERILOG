68a6333fe2b209270821f66eb717cae4cbfa0d4c
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-24 10:29:22 +0530

    Add comments for value and data pairs in constraint

---
+++ 19. CONSTRAINT/BASIC_CONSTRAINT/constraint_scoperesolution.sv
@@ -0,0 +1,27 @@
+class packet;
+  rand bit [3:0] value;
+  randc bit [3:0] data;
+  
+  constraint a1;
+  function void display();
+    $display("Value =%d, data=%d",value, data);
+  endfunction
+endclass
+
+constraint packet::a1 {value >14;data<10;};
+
+module name;
+  initial begin
+    packet p;
+    p=new();
+    repeat (4) begin
+      p.randomize();
+      p.display();
+    end
+  end
+endmodule
+
+# Value =15, data= 3
+# Value =15, data= 0
+# Value =15, data= 6
+# Value =15, data= 8
