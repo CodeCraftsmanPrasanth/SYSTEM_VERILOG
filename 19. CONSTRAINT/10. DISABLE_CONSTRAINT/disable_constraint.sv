c8356981eb9f57c85b430d4b8f65083b1eb11944
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-12-02 12:16:18 +0530

    Create disable_constraint.sv

---
+++ 19. CONSTRAINT/10. DISABLE_CONSTRAINT/disable_constraint.sv
@@ -0,0 +1,23 @@
+class seq_item;
+  rand bit [7:0] value1;
+  rand bit [7:0] value2;
+
+  constraint value1_c {value1 inside {[10:30]};}
+  constraint value2_c {value2 inside {40,70, 80};}
+
+endclass
+
+module ex;
+  initial begin
+    seq_item f;
+    f=new();
+    f.randomize();
+    $display("value1 =%0d, value2 = %0d",f.value1,f.value2);
+    f.value2_c.constraint_mode(0);
+    f.randomize();
+    $display("value1 =%0d, value2 = %0d",f.value1,f.value2);
+  end
+endmodule
+
+# value1 =20, value2 = 40
+# value1 =15, value2 = 95
