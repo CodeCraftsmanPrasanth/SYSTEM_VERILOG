17825d0af92e28b72972f5d81878a4152e58f3a2
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-12-04 10:42:17 +0530

    Update ex1.sv

--- 19. CONSTRAINT/11. STATIC/ex1.sv
+++ 19. CONSTRAINT/11. STATIC/ex1.sv
@@ -2,29 +2,35 @@ class seq_item;
   rand bit [7:0] value1;
   rand bit [7:0] value2;
 
-  constraint value1_c {value1 inside {[10:30]};}
-  static constraint value2_c {value2 inside {40,70, 80};}
+  constraint value1_c {value1 inside {[20:60]};}
+  static constraint value2_c {value2 inside {40,60, 80};}
 
 endclass
 
-module ex;
+module constraint_example;
+  seq_item item1, item2;
+  
   initial begin
-    seq_item f,g;
-    f=new();
-    g=new();
-    f.randomize();
-    g.randomize();
-    $display("f : value1 =%0d, value2 = %0d",f.value1,f.value2);
-    $display("g : value1 =%0d, value2 = %0d",g.value1,g.value2);
-    f.value2_c.constraint_mode(0);
-    f.randomize();
-    g.randomize();
-    $display("f : value1 =%0d, value2 = %0d",f.value1,f.value2);
-    $display("g : value1 =%0d, value2 = %0d",g.value1,g.value2);
+    item1 = new();
+    item2 = new();
+    
+    item1.randomize();
+    item2.randomize();
+    $display("Before disabling constraint");
+    $display("item1: value1 = %0d, value2 = %0d", item1.value1, item1.value2);
+    $display("item2: value1 = %0d, value2 = %0d", item2.value1, item2.value2);
+    
+    item2.value2_c.constraint_mode(0);  // To disable constraint for value2 using handle item2
+    item1.randomize();
+    item2.randomize();
+    $display("After disabling constraint for all value2 alone");
+    $display("item1: value1 = %0d, value2 = %0d", item1.value1, item1.value2);
+    $display("item2: value1 = %0d, value2 = %0d", item2.value1, item2.value2);
   end
 endmodule
 
-# f : value1 =20, value2 = 40
-# g : value1 =19, value2 = 80
-# f : value1 =15, value2 = 95
-# g : value1 =29, value2 = 194
+item1: value1 = 28, value2 = 60
+item2: value1 = 22, value2 = 60
+After disabling constraint for all value2 alone
+item1: value1 = 29, value2 = 63
+item2: value1 = 60, value2 = 72
